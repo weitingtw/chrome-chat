43a2f413b8402eefcefe080624775ccc82999281
Wei-Ting Chen <weitingchen310@hotmail.com>
2019-07-06 20:18:23 +0800

    version 1.1

--- app/background.js
+++ app/background.js
@@ -1,7 +1,125 @@
 var socket = 0;
-
+var currentPage = null;
 chrome.runtime.onMessage.addListener(messageReceived);
 
+var buffered_message = [];
+var buffered_status = "";
+var username = "";
+var is_close = false;
+var num_of_messages = 0;
+var popup_num = 0;
+
 function messageReceived(msg) {
-  console.log(msg);
+  switch (msg.instruction) {
+    case "disconnecting":
+      socket.emit("disconnecting");
+      socket = 0;
+      break;
+    case "connect":
+      socket = io("https://chrome-chat.herokuapp.com");
+      //socket = io("http://localhost:3000");
+      socket.on("connected", data => {
+        chrome.runtime.sendMessage({ instruction: "connected" });
+        buffered_status = "waiting for a match";
+      });
+      socket.on("match", data => {
+        chrome.runtime.sendMessage({ instruction: "displayPage", num: 3 });
+        currentPage = 3;
+      });
+      socket.on("disconnected", data => {
+        chrome.runtime.sendMessage({ instruction: "displayPage", num: 1 });
+        chrome.runtime.sendMessage({ instruction: "clear_screen" });
+        currentPage = 1;
+        var notifOptions = {
+          type: "basic",
+          iconUrl: "img/bad.png",
+          title: "Disconnected",
+          message: "Your chat has ended. Please find a new match."
+        };
+        chrome.notifications.create("end_chat", notifOptions);
+        socket = 0;
+        buffered_message = [];
+        chrome.notifications.clear("end_chat");
+      });
+      socket.on("chat", data => {
+        chrome.runtime.sendMessage({
+          instruction: "receive_message",
+          message: data.message,
+          name: data.name
+        });
+        buffered_message.push({
+          message: data.message,
+          name: data.name,
+          is_your_own_message: false
+        });
+        if (is_close) {
+          num_of_messages += 1;
+          chrome.browserAction.setBadgeText({
+            text: num_of_messages.toString()
+          });
+        }
+      });
+      break;
+    case "username":
+      username = msg.username;
+      socket.emit("join", { name: msg.username });
+      break;
+    case "send_message":
+      socket.emit("chat", {
+        message: msg.sent_message
+      });
+      buffered_message.push({
+        message: msg.sent_message,
+        name: username,
+        is_your_own_message: true
+      });
+      break;
+    case "clear_socket":
+      socket = 0;
+      break;
+    case "save_page":
+      currentPage = msg.page;
+      break;
+    case "get_page":
+      chrome.runtime.sendMessage({
+        instruction: "return_page",
+        page: currentPage
+      });
+      break;
+    case "get_buffered_message":
+      chrome.runtime.sendMessage({
+        instruction: "buffered_message",
+        message: buffered_message
+      });
+      break;
+    case "get_buffered_status":
+      chrome.runtime.sendMessage({
+        instruction: "buffered_status",
+        status: buffered_status
+      });
+      break;
+    case "reset_badge_text":
+      chrome.browserAction.setBadgeText({ text: "" });
+      num_of_messages = 0;
+      is_close = false;
+      break;
+    case "is_close":
+      is_close = true;
+      console.log("dsa");
+      break;
+    default:
+  }
 }
+chrome.runtime.onConnect.addListener(function(port) {
+  popup_num += 1;
+});
+chrome.runtime.onConnect.addListener(function(externalPort) {
+  externalPort.onDisconnect.addListener(function() {
+    popup_num -= 1;
+    if (popup_num == 0) {
+      is_close = true;
+    }
+  });
+
+  console.log("onConnect");
+});
--- script.js
+++ script.js
@@ -35,6 +35,8 @@ function displayPage(index) {
       pages[i].style.display = "none";
     }
   }
+  chrome.runtime.sendMessage({ instruction: "save_page", page: index });
+  //chrome.storage.sync.set({ current_page: index }, function() {});
 }
 
 function remove_children(element) {
@@ -58,7 +60,7 @@ function add_chat_message(
     div.className = "container";
   }
   p.innerHTML = message;
-  p2.innerHTML = name + ":";
+  p2.innerHTML = name + ":   ";
   div.appendChild(p2);
   div.appendChild(p);
 
@@ -79,11 +81,13 @@ message_box.addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
     if (message.value != "") {
       var sent_message = message.value;
-      socket.emit("chat", {
-        message: sent_message
+      chrome.runtime.sendMessage({
+        instruction: "send_message",
+        sent_message: sent_message
       });
       chrome.storage.sync.get(["username"], function(data) {
         add_chat_message(true, message_channel, sent_message, data.username);
+        updateScroll();
       });
     }
     message_box.value = "";
@@ -96,44 +100,60 @@ btn_back.addEventListener("click", function() {
 
 btn_back_2.addEventListener("click", function() {
   displayPage(1);
-  socket.emit("disconnecting");
-  socket = 0;
+  chrome.runtime.sendMessage({ instruction: "disconnecting" });
+  chrome.runtime.sendMessage({ instruction: "clear_socket" });
 });
 
 btn_back_3.addEventListener("click", function() {
-  socket.emit("disconnecting");
-  socket = 0;
+  chrome.runtime.sendMessage({ instruction: "disconnecting" });
+  chrome.runtime.sendMessage({ instruction: "clear_socket" });
 });
 
 btn_match.addEventListener("click", function() {
   displayPage(2);
-  socket = io("http://localhost:3000");
-  chrome.storage.sync.get(["username"], function(data) {
-    socket.emit("join", { name: data.username });
-  });
+  header3.innerHTML = "Connecting";
+  chrome.runtime.sendMessage({ instruction: "connect" });
 
-  socket.on("connected", data => {
-    header3.innerHTML = "connected, looking for other users ";
+  chrome.storage.sync.get(["username"], function(data) {
+    chrome.runtime.sendMessage({
+      instruction: "username",
+      username: data.username
+    });
   });
-  socket.on("match", data => {
-    displayPage(3);
-  });
-  socket.on("disconnected", data => {
-    displayPage(1);
+});
+
+chrome.runtime.onMessage.addListener(function(msg) {
+  if (msg.instruction == "connected") {
+    header3.innerHTML = "waiting for a match";
+  } else if (msg.instruction == "displayPage") {
+    displayPage(parseInt(msg.num));
+  } else if (msg.instruction == "clear_screen") {
     remove_children(message_channel);
-    var notifOptions = {
-      type: "basic",
-      iconUrl: "img/bad.png",
-      title: "Disconnected",
-      message: "Your chat has ended. Please find a new match."
-    };
-    chrome.notifications.create("end_chat", notifOptions);
-    socket = 0;
-  });
-  socket.on("chat", data => {
-    add_chat_message(false, message_channel, data.message, data.name);
-    console.log(data.message);
-  });
+  } else if (msg.instruction == "receive_message") {
+    add_chat_message(false, message_channel, msg.message, msg.name);
+    updateScroll();
+  } else if (msg.instruction == "return_page") {
+    if (msg.page) {
+      displayPage(msg.page);
+    }
+    if (msg.page == 3) {
+      chrome.runtime.sendMessage({ instruction: "get_buffered_message" });
+    } else if (msg.page == 2) {
+      chrome.runtime.sendMessage({ instruction: "get_buffered_status" });
+    }
+  } else if (msg.instruction == "buffered_message") {
+    var buffered_message = msg.message;
+    buffered_message.forEach(item => {
+      add_chat_message(
+        item.is_your_own_message,
+        message_channel,
+        item.message,
+        item.name
+      );
+    });
+  } else if (msg.instruction == "buffered_status") {
+    header3.innerHTML = msg.status;
+  }
 });
 
 chrome.storage.sync.get(["username"], function(data) {
@@ -146,8 +166,22 @@ chrome.storage.sync.get(["username"], function(data) {
   }
 });
 
-chrome.runtime.sendMessage({ greeting: "hello" }, function(response) {});
+chrome.runtime.sendMessage({ instruction: "get_page" });
+chrome.runtime.sendMessage({ instruction: "reset_badge_text" });
+resize = function() {
+  document.getElementById("body").style.width = "200px";
+  document.getElementById("body").style.height = "300px";
+};
+updateScroll = function() {
+  var element = document.getElementById("message-channel");
+  element.scrollTop = element.scrollHeight;
+};
 
-chrome.runtime.onMessage.addListener(function(msg) {
-  console.log(msg);
-});
+onload = function() {
+  setTimeout(resize, 300);
+  setTimeout(updateScroll, 10);
+};
+
+document.onload = onload;
+
+var port = chrome.runtime.connect({});
